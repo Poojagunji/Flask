9a5ebaa859537034c95c593869d36356bf3affaa
Poojitha Gunji <pgunji@innominds.com>
2021-11-25 15:24:10 +0530

    added register template and routing

--- app.py
+++ app.py
@@ -9,5 +9,9 @@ def hello_world():
 @app.route('/login')
 def Login():
     return render_template("login.html")
+
+@app.route('/register')
+def Register():
+    return render_template("register.html")
 if __name__ == "__main__":
     app.run(debug=True )
