66305098e8f6eca6d1374419a371098f272891d8
Poojitha Gunji <pgunji@innominds.com>
2021-11-26 11:57:55 +0530

    added dashboard

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
 from flask import Flask , render_template,url_for,request,session,redirect
 import pymongo
-import bcrypt
+
 
 app = Flask(__name__)
 client = pymongo.MongoClient("mongodb+srv://pooja:Pooja2000@cluster0.krtlz.mongodb.net/detaisl?retryWrites=true&w=majority")
 db = client.get_database('total_records')
 records = db.register
-# url mongodb+srv://pooja:Pooja2000@cluster0.krtlz.mongodb.net/detaisl?retryWrites=true&w=majority
-# app.config['MONGO_DBNAME']='detaisl'
-# app.config['MONGO_URI']='mongodb+srv://pooja:Pooja2000@cluster0.krtlz.mongodb.net/detaisl?retryWrites=true&w=majority'
-# mongo=PyMongo(app)
 
 @app.route("/")
 def index():
@@ -22,6 +18,7 @@ def index():
 def registerRoute():
     return render_template('register.html')
 
+
 @app.route('/login',methods=['POST'])
 def login():
     username=request.form.get("username")
@@ -32,18 +29,19 @@ def login():
         return "data found"
     else:
         return "not found"
-    # users = mongo.db.users
-    # login_user = users.find_one({'name' : request.form['username']})
-
-    # if login_user:
-    #     if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password'].encode('utf-8')) == login_user['password'].encode('utf-8'):
-    #         session['username'] = request.form['username']
-    #         return redirect(url_for('index'))
-    # return 'Invalid username or password combination'
     return ""
 
+@app.route('/login')
+def reg():
+    return render_template('login.html')
 
-@app.route('/register',methods=["GET","POST"])
+@app.route('/dashboard')
+def dash():
+    return render_template("dashboard.html")
+
+
+    
+@app.route('/register',methods=["GET","POST" ,])
 def register():
     firstname=request.form.get("firstname")
     lastname=request.form.get("lastname")
@@ -51,17 +49,7 @@ def register():
     password=request.form.get("password")
     confirmPassword=request.form.get("confirmPassword")
     print(firstname,lastname,email,password,confirmPassword)
-
     records.insert_one({"email":email,"password":password,"firstname":firstname,"lastname":lastname})
-
-    
-    # if (validateEmail(email) and  password_check(password) and check_phonenumber(phonenumber)):
-    #     register.insert_one({"email":email,"phone_no":phonenumber,"user_name":username,"password":password})
-    #     # print(username,password,phonenumber,email)
-    #     return render_template("login.html")
-    # else:
-    #     return render_template("error.html")
-
     return render_template('login.html')
 
 
