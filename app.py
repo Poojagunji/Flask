290d1b97e5eef1f3f9cb1a8d523c5ca48db31a29
Poojitha Gunji <pgunji@innominds.com>
2021-11-28 15:12:35 +0530

    blogs posted to db

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask , render_template,url_for,request,session,redirect
-import pymongo
+from flask import *
+from flask_pymongo import PyMongo
 
 
 app = Flask(__name__)
-client = pymongo.MongoClient("mongodb+srv://pooja:Pooja2000@cluster0.krtlz.mongodb.net/detaisl?retryWrites=true&w=majority")
-db = client.get_database('total_records')
+# client = pymongo.MongoClient("mongodb+srv://pooja:Pooja2000@cluster0.krtlz.mongodb.net/detaisl?retryWrites=true&w=majority")
+# db = client.get_database('total_records')
+# records = db.register
+
+mongo_client = PyMongo(app,uri="mongodb+srv://pooja:pooja2000@cluster0.krtlz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+db = mongo_client.db
 records = db.register
 
 @app.route("/")
@@ -14,7 +18,7 @@ def index():
 
     return render_template('index.html')
 
-@app.route("/register")
+@app.route('/register')
 def registerRoute():
     return render_template('register.html')
 
@@ -46,12 +50,38 @@ def register():
     firstname=request.form.get("firstname")
     lastname=request.form.get("lastname")
     email=request.form.get("email")
+    number=request.form.get("number")
     password=request.form.get("password")
     confirmPassword=request.form.get("confirmPassword")
-    print(firstname,lastname,email,password,confirmPassword)
-    records.insert_one({"email":email,"password":password,"firstname":firstname,"lastname":lastname})
+    print(firstname,lastname,email,password,confirmPassword,number)
+    records.insert_one({"email":email,"password":password,"firstname":firstname,"lastname":lastname,"number":number})
     return render_template('login.html')
 
+
+@app.route('/forgotpassword')
+def forgotpassword():
+    return render_template("forgotpassword.html")
+
+@app.route('/blogpage',methods=['GET','POST'])
+def blogpage():
+     
+    if(request.method=='POST'):
+        if(request.form):
+            name = request.form.get("blogName")
+            story = request.form.get("story")
+            ID = request.form.get("id")
+            print(name)
+            print(story)
+            print(ID)
+            records.insert_one({"name":name,"story":story,"id":id})
+            return "posted successfully"
+        else:
+           return "please fill all fields" 
+    return render_template('blogpage.html')
+
+@app.route('blogsdashboard')
+def dashboard():
+    return render_template('blogsdashboard.html')
 
 
 if __name__=='__main__':
