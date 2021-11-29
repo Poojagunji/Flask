1c249a67c7824f25c2e2e142deaac2ad2789520f
Poojitha Gunji <pgunji@innominds.com>
2021-11-29 10:42:47 +0530

    changes in the code

--- app.py
+++ app.py
@@ -7,54 +7,58 @@ app = Flask(__name__)
 # db = client.get_database('total_records')
 # records = db.register
 
-mongo_client = PyMongo(app, uri="mongodb+srv://pooja:pooja2000@cluster0.krtlz.mongodb.net/total_records?retryWrites=true&w=majority")
+mongo_client = PyMongo(
+    app, uri="mongodb+srv://pooja:Pooja2000@cluster0.krtlz.mongodb.net/blogs?retryWrites=true&w=majority")
 db = mongo_client.db
-records = db.register
+records = db.blog
+
 
 @app.route("/")
 def index():
     if 'username' in session:
-        return 'You are logged as the following user:' +session['username']
+        return 'You are logged as the following user:' + session['username']
 
     return render_template('index.html')
+
 
 @app.route('/register')
 def registerRoute():
     return render_template('register.html')
 
 
-@app.route('/login',methods=['POST'])
+@app.route('/login', methods=['POST'])
 def login():
-    username=request.form.get("username")
-    password=request.form.get("password")
-    print(username,password)
-    finding=records.find_one({"email":username,"password":password})
+    username = request.form.get("username")
+    password = request.form.get("password")
+    print(username, password)
+    finding = records.find_one({"email": username, "password": password})
     if(finding):
         return render_template('dashboard.html')
     else:
         return " user not found"
-    
 
-@app.route('/login')
-def reg():
-    return render_template('login.html')
+
+# @app.route('/login')
+# def reg():
+#     return render_template('login.html')
+
 
 @app.route('/dashboard')
 def dash():
     return render_template("dashboard.html")
 
 
-
-@app.route('/register',methods=["GET","POST" ,])
+@app.route('/register', methods=["GET", "POST", ])
 def register():
-    firstname=request.form.get("firstname")
-    lastname=request.form.get("lastname")
-    email=request.form.get("email")
-    number=request.form.get("number")
-    password=request.form.get("password")
-    confirmPassword=request.form.get("confirmPassword")
-    print(firstname,lastname,email,password,confirmPassword,number)
-    records.insert_one({"email":email,"password":password,"firstname":firstname,"lastname":lastname,"number":number})
+    firstname = request.form.get("firstname")
+    lastname = request.form.get("lastname")
+    email = request.form.get("email")
+    number = request.form.get("number")
+    password = request.form.get("password")
+    confirmPassword = request.form.get("confirmPassword")
+    print(firstname, lastname, email, password, confirmPassword, number)
+    records.insert_one({"email": email, "password": password,
+                       "firstname": firstname, "lastname": lastname, "number": number})
     return render_template('login.html')
 
 
@@ -62,19 +66,20 @@ def register():
 def forgotpassword():
     return render_template("forgotpassword.html")
 
-@app.route('/postpage',methods=['GET','POST'])
+
+@app.route('/postpage', methods=['GET', 'POST'])
 def blogpage():
-     
-    if(request.method=='POST'):
+    if(request.method == 'POST'):
         if(request.form):
             print(dict(request.form))
-            records.insert_one(dict(request.form))
+            db.blog.insert_many([dict(request.form)])
             return "posted successfully"
         else:
-           return "please fill all fields" 
+            return "please fill all fields"
     return render_template('blogpage.html')
 
-@app.route('/blogsdashboard',methods=['GET'])
+
+@app.route('/blogsdashboard', methods=['GET'])
 def dashboard():
     return render_template('blogsdashboard.html')
 
@@ -83,13 +88,18 @@ def dashboard():
 def update():
     return "update page"
 
-@app.route('/deletepage')
+
+@app.route('/delete', methods=['GET','POST'])
 def delete():
-    return "Delete page"
+	if(request.method == 'POST'):
+		id = request.form
+		print(dict(id))
+		res = db.blog.delete_many([dict(id)])
+		return res
+	else:
+		return render_template('delete.html')
 
 
-if __name__=='__main__':
-    app.secret_key='secretivekeyagain'
+if __name__ == '_main_':
+    app.secret_key = 'secretivekeyagain'
     app.run(debug=True)
-
- 
